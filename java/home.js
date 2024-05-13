d5c3bb668459b23d371ba7e4a22042e96a36d6f8
JustHuman1805 <howjl1850@gmail.com>
2024-05-13 16:08:12 +0700

    jadi wa udah ngerti

--- java/home.js
+++ java/home.js
@@ -12,3 +12,14 @@ document.addEventListener('click',function(ev){
         search.classList.remove('active')
     }
 })
+const menubut = document.getElementById('menubut')
+const nav = document.querySelector('.navigasi')
+menubut.addEventListener('click',function(ev){
+    nav.classList.toggle('active')
+    ev.preventDefault()
+})
+document.addEventListener('click',function(ev){
+    if(!menubut.contains(ev.target) && !nav.contains(ev.target)){
+        nav.classList.remove('active')
+    }
+})
